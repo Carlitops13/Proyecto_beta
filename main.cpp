575501b331ac6fc4b394846eb6b89e507cfcac2d
lubswer <lubswer@hotmail.com>
2025-07-07 16:38:38 -0500

    Cambio Lubswer 7 de julio
    hice un texto, otra ventana y titulo en la ventana con texto, agregue Label

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
     QTranslator translator;
+    QLabel *label = new QLabel("Andrea");
+    label -> show();
+    label -> resize(200,100);
+    label -> setWindowTitle("Mi ventana Lubswer Cambio");
+
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
         const QString baseName = "Proyecto_Alfa_" + QLocale(locale).name();
